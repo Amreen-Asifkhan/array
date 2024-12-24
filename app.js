4c115a6acf50de1d0d56e5fa5798801e202ac8fa
Amreen Asif  Khan <amreenasifkhan1@gmail.com>
2024-12-23 22:29:43 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var myArray=[80,50,40,57,29];
+myArray.sort();
+console.log('largest'+myArray[myArray.length-2]);
+
+
+
+
+
+
+
+// =======================
+var myArray=[20,2,4,53,29,3,39,48];
+myArray.sort();
+console.log(myArray);
